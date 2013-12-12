1635de0d2063a1f372205a60829afc0d845df524
bsspirit <bsspirit@163.com>
2013-12-13 00:30:26 +0800

    'json2csv'

--- btc-spider/service/service.js
+++ btc-spider/service/service.js
@@ -11,7 +11,7 @@ var _this = this;
  */
 exports.pushBTChinaHistory = function (obj, callback) {
     dao.pushBTChinaHistory(obj, function (err, rows) {
-        if (err) console.log("pushBTChinaHistory Error ==> " + err);
+        if (err) console.log('pushBTChinaHistory Error ==> ' + err);
     });
 }
 
@@ -26,12 +26,12 @@ exports.pushBTChinaHistoryArr = function (list, callback) {
  */
 exports.pushBTChinaTrade = function (obj, callback) {
     dao.pushBTChinaTrade(obj, function (err, rows) {
-        if (err) console.log("pushBTChinaTrade Error ==> " + err);
+        if (err) console.log('pushBTChinaTrade Error ==> ' + err);
     });
 }
 
 exports.pushBTChinaTrade_redis = function (obj, callback) {
-    json2csv({data: obj, fields: ['tid', 'datetime',"price",'count',"operate"]}, function(err, csv) {
+    json2csv({data: obj, fields: ['tid', 'datetime','price','count','operate']}, function(err, csv) {
         if (err) console.log(err);
         dao.pushBTChinaTrade_redis(csv);
     });
@@ -42,13 +42,13 @@ exports.pushBTChinaTrade_redis = function (obj, callback) {
  */
 exports.pushBTChinaDepth = function (obj, callback) {
 
-    json2csv({data: obj, fields: ['datetime', 'ask1',"ask1_size",'ask2',"ask2_size",'ask3',"ask3_size",'ask4',"ask4_size",'ask5',"ask5_size",
-        'bid1',"bid1_size", 'bid2',"bid2_size", 'bid3',"bid3_size", 'bid4',"bid4_size", 'bid5',"bid5_size"]}, function(err, csv) {
+    json2csv({data: obj, fields: ['datetime', 'ask1','ask1_size','ask2','ask2_size','ask3','ask3_size','ask4','ask4_size','ask5','ask5_size',
+        'bid1','bid1_size', 'bid2','bid2_size', 'bid3','bid3_size', 'bid4','bid4_size', 'bid5','bid5_size']}, function(err, csv) {
         if (err) console.log(err);
         dao.pushBTChinaDepth_redis(csv);
     });
 
     dao.pushBTChinaDepth(obj, function (err, rows) {
-        if (err) console.log("pushBTChinaDepth Error ==> " + err);
+        if (err) console.log('pushBTChinaDepth Error ==> ' + err);
     });
 }
