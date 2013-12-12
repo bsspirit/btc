2173b1d82233f9ad607a0be2bb55789e6387cffc
bsspirit <bsspirit@163.com>
2013-12-13 00:26:49 +0800

    'json2csv'

--- btc-spider/service/service.js
+++ btc-spider/service/service.js
@@ -2,7 +2,8 @@
 //biz service layer
 
 var dao = require('./dao')
-    ,utils = require('./utils');
+    ,utils = require('./utils')
+    ,json2csv = require('json2csv');
 var _this = this;
 
 /**
@@ -30,14 +31,23 @@ exports.pushBTChinaTrade = function (obj, callback) {
 }
 
 exports.pushBTChinaTrade_redis = function (obj, callback) {
-    dao.pushBTChinaTrade_redis(utils.JSON2CSV(obj));
+    json2csv({data: obj, fields: ['tid', 'datetime',"price",'count',"operate"]}, function(err, csv) {
+        if (err) console.log(err);
+        dao.pushBTChinaTrade_redis(csv);
+    });
 }
 
 /**
  * 时时询价数据
  */
 exports.pushBTChinaDepth = function (obj, callback) {
-    dao.pushBTChinaDepth_redis(utils.JSON2CSV(obj));
+
+    json2csv({data: obj, fields: ['datetime', 'ask1',"ask1_size",'ask2',"ask2_size",'ask3',"ask3_size",'ask4',"ask4_size",'ask5',"ask5_size",
+        'bid1',"bid1_size", 'bid2',"bid2_size", 'bid3',"bid3_size", 'bid4',"bid4_size", 'bid5',"bid5_size"]}, function(err, csv) {
+        if (err) console.log(err);
+        dao.pushBTChinaDepth_redis(csv);
+    });
+
     dao.pushBTChinaDepth(obj, function (err, rows) {
         if (err) console.log("pushBTChinaDepth Error ==> " + err);
     });
