3854839d5055393746b1782313ce9b8e37b9e3b8
Conan Zhang <bsspirit@163.com>
2013-12-17 19:19:04 +0800

    Update realtime.js

--- btc-spider/client/realtime.js
+++ btc-spider/client/realtime.js
@@ -11,10 +11,10 @@ $(document).ajaxComplete(function(event,request, settings){
 
     var ip = 'http://42.121.108.236:3001/';
 
-    if(settings.url.indexOf("/sdepth?symbol=btcchinabtccny")>0){//sdepth
-        var obj = {return:request.responseText}
-        send(ip+'api/bt/china/depth/push',obj);
-    }
+    //if(settings.url.indexOf("/sdepth?symbol=btcchinabtccny")>0){//sdepth
+    //    var obj = {return:request.responseText}
+    //    send(ip+'api/bt/china/depth/push',obj);
+    //}
 
     if(settings.url.indexOf("stock?method=tid")>0){//trade
         var obj = {return:request.responseText}
