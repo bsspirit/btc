e0ade55019150101e1e23f214846a19d130ab157
Conan Zhang <bsspirit@163.com>
2013-12-19 14:41:02 +0800

    update 5min flush

--- btc-spider/client/realtime.js
+++ btc-spider/client/realtime.js
@@ -11,14 +11,20 @@ $(document).ajaxComplete(function(event,request, settings){
 
     var ip = 'http://42.121.108.236:3001/';
 
-    //if(settings.url.indexOf("/sdepth?symbol=btcchinabtccny")>0){//sdepth
-    //    var obj = {return:request.responseText}
-    //    send(ip+'api/bt/china/depth/push',obj);
-    //}
+//    if(settings.url.indexOf("/sdepth?symbol=btcchinabtccny")>0){//sdepth
+//        var obj = {return:request.responseText}
+//        send(ip+'api/bt/china/depth/push',obj);
+//    }
 
     if(settings.url.indexOf("stock?method=tid")>0){//trade
-        var obj = {return:request.responseText}
-        send(ip+'api/bt/china/trade/push',obj);
+        if(request.responseText.length>0){
+            var obj = {return:request.responseText};
+            send(ip+'api/bt/china/trade/push',obj);
+        }
     }
 
 });
+
+setTimeout(function(){
+    location.href=location.href
+},1000*5*60)
